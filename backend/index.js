a750128856b8867f2049d684c5d63925bc4987b1
Aaron Balke <aabalke33@gmail.com>
2023-10-24 20:16:29 -0600

    Adjust Node

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 8008
+import express from 'express'
+const app = express();
+const port = 8008;
 
 
 app.get('/', (req, res) => {
